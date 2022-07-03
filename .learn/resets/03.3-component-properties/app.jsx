f5c6871663a9cff2bcf75e141c8895a86730c3cc
kjdixon74 <kjdixon500@gmail.com>
2022-07-03 02:52:07 +0000

    uploading my code

--- .learn/resets/03.3-component-properties/app.jsx
+++ .learn/resets/03.3-component-properties/app.jsx
@@ -6,12 +6,12 @@ const BootstrapCard = props => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
-			<img className="card-img-top" src={props.imageURL} alt="Card image cap" />
+			<img className="card-img-top" src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true" alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">{props.description}</p>
-				<a href={props.buttonUrl} className="btn btn-primary">
-					{props.buttonLabel}
+				<p className="card-text">Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.</p>
+				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
+					Go to wikipedia
 				</a>
 			</div>
 		</div>
